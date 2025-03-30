f8f63db52e7b6aaec3a420ef999422399b3d24cd
Denis mgaya <dnspascal094@gmail.com>
2025-03-30 15:57:03 +0300

    chatting

--- mediasoup-config.js
+++ mediasoup-config.js
@@ -1,25 +1,23 @@
-module.exports = {
-    worker: {
-      rtcMinPort: 40000,
-      rtcMaxPort: 49999,
+export const worker = {
+  rtcMinPort: 40000,
+  rtcMaxPort: 49999,
+};
+export const router = {
+  mediaCodecs: [
+    {
+      kind: 'audio',
+      mimeType: 'audio/opus',
+      clockRate: 48000,
+      channels: 2,
     },
-    router: {
-      mediaCodecs: [
-        {
-          kind: 'audio',
-          mimeType: 'audio/opus',
-          clockRate: 48000,
-          channels: 2,
-        },
-        {
-          kind: 'video',
-          mimeType: 'video/VP8',
-          clockRate: 90000,
-          parameters: {
-            'x-google-start-bitrate': 1000,
-          },
-        },
-      ],
+    {
+      kind: 'video',
+      mimeType: 'video/VP8',
+      clockRate: 90000,
+      parameters: {
+        'x-google-start-bitrate': 1000,
+      },
     },
-  };
+  ],
+};
   
