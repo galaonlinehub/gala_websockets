dd3104e6a2605b0ce28f9fe2ce652d6f22c77fdf
Denis mgaya <dnspascal094@gmail.com>
2025-04-26 16:28:31 +0300

    redis url

--- src/services/redis.js
+++ src/services/redis.js
@@ -8,7 +8,6 @@ let redisSubscriber = null;
 export async function initRedisClient() {
   if (redisClient) return redisClient;
 
-  logger.info("URL", config.redis.url);
   redisClient = createClient({
     url: config.redis.url,
 
