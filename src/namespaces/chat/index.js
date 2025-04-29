fe35a64e295b1f57aff6e8a7d6db2bc33ddbbaf4
Denis mgaya <dnspascal094@gmail.com>
2025-04-29 07:50:15 +0300

    redis url

--- src/namespaces/chat/index.js
+++ src/namespaces/chat/index.js
@@ -3,6 +3,7 @@ import { logger } from "../../utils/logger.js";
 import { chatSocket } from "./socket.js";
 
 export function setupChatNamespace(io, redisClient) {
+  logger.info(`THIS IS GOOD ${io}`);
   const chatNamespace = authenticateNamespace(io.of("/chat"));
 
   chatSocket(chatNamespace, redisClient);
