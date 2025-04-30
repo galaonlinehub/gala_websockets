4e46907bb366e5e370d3cd21a4fb157621b92fde
Denis mgaya <dnspascal094@gmail.com>
2025-05-01 01:36:47 +0300

    Auth token

--- src/namespaces/payment/index.js
+++ src/namespaces/payment/index.js
@@ -3,8 +3,8 @@ import { logger } from "../../utils/logger.js";
 import { paymentSocket } from "./socket.js";
 
 export function setupPaymentNamespace(io, redisClient) {
-  const paymentNamespace = authenticateNamespace(io.of("/payment"));
-
+  // const paymentNamespace = authenticateNamespace(io.of("/payment"));
+  const paymentNamespace = io.of("/payment");
   paymentSocket(paymentNamespace, redisClient);
 
   logger.info("Payment namespace initialized");
