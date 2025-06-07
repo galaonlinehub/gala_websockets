f70eb5e5ffead0fb3e5e6a5f15cf4d104d2c1e00
Denis mgaya <dnspascal094@gmail.com>
2025-06-07 04:22:48 +0300

    result logged

--- src/namespaces/chat/repository.js
+++ src/namespaces/chat/repository.js
@@ -15,8 +15,7 @@ export const getParticipantsWithFallback = async ({
     pinnoLogger.info("Redis has no participants, falling back to API...");
 
     const apiParticipants = await getParticipantsApi(chatId, context);
-    pinnoLogger.info({ msg: "Charts in repo", chats: apiParticipants });
-    participants = apiParticipants.map((p) => p.user_id).filter(Boolean);
+    participants = apiParticipants.filter(Boolean);
 
     if (participants.length > 0) {
       pinnoLogger.info("WE IN IN REDIS");
