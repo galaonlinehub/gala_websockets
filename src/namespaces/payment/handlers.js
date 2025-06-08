c3740aaeccd3bb06ff04832deb57d5ea30d5930f
Denis mgaya <dnspascal094@gmail.com>
2025-06-08 18:23:04 +0300

    message

--- src/namespaces/payment/handlers.js
+++ src/namespaces/payment/handlers.js
@@ -2,6 +2,7 @@ import pinnoLogger from "../../utils/pinno-logger.js";
 
 export const handleJoin = (s, { id }) => {
   if (id) {
+    console.log("User joined",id);
     s.join(id);
   } else {
     pinnoLogger.warn("No email provided in join event.");
