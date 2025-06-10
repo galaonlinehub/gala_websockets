9ce54e67351807e8264e86cd2ad53cd1c52d5648
Denis mgaya <dnspascal094@gmail.com>
2025-06-10 17:22:33 +0300

    Sent status in message

--- src/namespaces/chat/handlers.js
+++ src/namespaces/chat/handlers.js
@@ -153,6 +153,8 @@ export async function handleSendMessage({ socket, data, namespace, redisOps }) {
       });
     }
 
+    pinnoLogger.debug(deliveredUserIds, "users online")
+
     const unreadCountsPayload = [];
 
     for (const participantId of participants) {
